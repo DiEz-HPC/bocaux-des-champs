e5b41c0d358a3847b944aea85e747b66916289ab
tennessee <houry9@gmail.com>
2022-08-29 13:57:07 +0200

    add qr code options

--- src/QrCodeController.php
+++ src/QrCodeController.php
@@ -2,11 +2,9 @@
 
 namespace App;
 
-use Bolt\Entity\Content;
 use App\Service\QrGenerator;
 use Bolt\Controller\TwigAwareController;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,10 +14,6 @@ use Bolt\Controller\Backend\BackendZoneInterface;
 
 class QrCodeController extends TwigAwareController implements BackendZoneInterface
 {
-
-    /** @var EntityManagerInterface */
-    private $objectManager;
-
     public function __construct(EntityManagerInterface $objectManager)
     {
         $this->objectManager = $objectManager;
@@ -29,20 +23,16 @@ class QrCodeController extends TwigAwareController implements BackendZoneInterfa
     #[Route("qr-code/", name: "app_qr_code")]
     public function viewEdits(): Response
     {
-        // content_user.html.twig is a custom file 
-        // that needs to be located in the `templates`
-        // folder in the root of your project.
-        return $this->render('content_QrCode.html.twig', [
-            'title' => 'User content'
-        ]);
+       return $this->render('content_QrCode.html.twig');
     }
 
     #[Route("/qr-code/generate", name: "app_qr_code_generate")]
     public function gerenateQr(Request $request, QrGenerator $qrGenerator)
     {
         $data = $request->request->all();
-        if (!empty($data)) {
-            $qrGenerator->getQRs($data);
+
+        if (!empty($data)) {         
+            $qrGenerator->parseFormData($data);
             $response = new Response();
             $response->headers->set('Content-Type', 'application/zip');
             $response->headers->set('Content-Disposition', 'attachment;filename="qrCode.zip"');
--- src/Service/QrGenerator.php
+++ src/Service/QrGenerator.php
@@ -3,15 +3,14 @@
 namespace App\Service;
 
 use ZipArchive;
-use Bolt\Entity\Content;
 use Endroid\QrCode\Builder\BuilderInterface;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\Response;
 use Endroid\QrCodeBundle\Response\QrCodeResponse;
 
 class QrGenerator
 {
     private BuilderInterface $qrBuilder;
+
     const QR_CODE_ZIP_FILE = '/tmp/qrCode/qrCode.zip';
     const QR_CODE_IMAGE_DIRECTORY = '/tmp/qrCode/images';
 
@@ -20,87 +19,101 @@ class QrGenerator
         $this->qrBuilder = $customQrCodeBuilder;
     }
 
-    public function getQRs(array $slugs): array
-    {       
-        return $this->generateLink($slugs);
+    public function parseFormData(array $data)
+    {
+        $options = [
+            'size' => 400,
+            'label' => '',
+            'margin' => 20,
+        ];
+        foreach ($data as $key => $value) {
+            if ($key == 'qrCodeSize') {
+                $options['size'] = $value;
+                unset($data[$key]);
+            } elseif ($key == 'LabelQrCode') {
+                $options['label'] = $value;
+                unset($data[$key]);
+            } elseif ($key == 'qrCodeMargin') {
+                $options['margin'] = $value;
+                unset($data[$key]);
+            }
+        }
+        $this->getQRs($data, $options);
+    }
+
+    private function getQRs(array $slugs, array $options): array
+    {
+        return $this->generateLink($slugs, $options);
     }
 
 
-    private function generateLink(array $slugs): array
+    private function generateLink(array $slugs, array $options): array
     {
         $serverLink = $_SERVER['SYMFONY_DEFAULT_ROUTE_URL'];
         $qrcodes = [];
         foreach ($slugs as $slug => $contentType) {
             $link = $serverLink . $contentType . '/' . $slug;
-            $qrcodes[$slug] = $this->generateQr($link, $slug);
+            $qrcodes[$slug] = $this->generateQr($link, $slug, $options);
         }
         $this->ZipFile();
         return $qrcodes;
     }
 
-    private function generateQr(string $link, string $slug)
+    private function generateQr(string $link, string $slug, array $options)
     {
+        if ($options['label'] === 'moviesName') {
+            $options['label'] = $slug;
+        } else if ($options['label'] === 'websiteName') {
+            $options['label'] = $_SERVER['SERVER_NAME'];
+        }
         $result = $this->qrBuilder
-            ->size(400)
-            ->margin(20)
+            ->size($options['size'])
+            ->margin($options['margin'])
             ->data($link)
+            ->labelText($options['label'])
             ->build();
         $qrCode = new QrCodeResponse($result);
-       $this->createImage($qrCode, $slug);
-         return $qrCode;
+        $this->createImage($qrCode, $slug);
+        return $qrCode;
     }
 
-    private function createImage(QrCodeResponse $qrCode, string $slug){
+    private function createImage(QrCodeResponse $qrCode, string $slug)
+    {
         $filesystem = new Filesystem();
-       if(!$filesystem->exists(self::QR_CODE_IMAGE_DIRECTORY)){
-              $filesystem->mkdir(self::QR_CODE_IMAGE_DIRECTORY);
-       }
-       $filesystem->touch(self::QR_CODE_IMAGE_DIRECTORY.'/'.$slug.'.png');
-       $filesystem->appendToFile(self::QR_CODE_IMAGE_DIRECTORY.'/'.$slug.'.png', $qrCode->getContent());
+        if (!$filesystem->exists(self::QR_CODE_IMAGE_DIRECTORY)) {
+            $filesystem->mkdir(self::QR_CODE_IMAGE_DIRECTORY);
+        }
+        $filesystem->touch(self::QR_CODE_IMAGE_DIRECTORY . '/' . $slug . '.png');
+        $filesystem->appendToFile(self::QR_CODE_IMAGE_DIRECTORY . '/' . $slug . '.png', $qrCode->getContent());
     }
 
-    private function ZipFile(){
-        if(file_exists(self::QR_CODE_ZIP_FILE)){
+    private function ZipFile()
+    {
+        if (file_exists(self::QR_CODE_ZIP_FILE)) {
             unlink(self::QR_CODE_ZIP_FILE);
         }
         touch(self::QR_CODE_ZIP_FILE);
 
-        $pathdir = self::QR_CODE_IMAGE_DIRECTORY."/";
+        $pathdir = self::QR_CODE_IMAGE_DIRECTORY . "/";
         $dir = opendir($pathdir);
-        
+
         $zip = new ZipArchive();
         $zip->open(self::QR_CODE_ZIP_FILE);
-        while($file = readdir($dir)) {
-            if(is_file($pathdir.$file)) {
-               $zip -> addFile($pathdir.$file, $file);
+        while ($file = readdir($dir)) {
+            if (is_file($pathdir . $file)) {
+                $zip->addFile($pathdir . $file, $file);
             }
-         }
+        }
         $zip->close();
     }
 
-    public function purgeDirectories(){
-        $files = glob(self::QR_CODE_IMAGE_DIRECTORY.'/*');
-        foreach($files as $file){ // iterate files
-            if(is_file($file)) {
-              unlink($file); // delete file
-            }
-          }
-    }
-
-
-    public function get()
+    public function purgeDirectories()
     {
-        $om = $this->objectManager;
-        $qb = $om->createQueryBuilder();
-        $qb->select("ft.value")
-            ->from(Content::class, 't')
-            ->where("t.contentType = 'videos'")
-            ->join(Field::class, 'f', 'WITH', 'f.content = t.id')
-            ->andWhere("f.name = 'slug'")
-            ->join(FieldTranslation::class, 'ft', 'WITH', 'ft.translatable = f.id')
-            ->orderBy("t.id");
-        $query = $qb->getQuery();
-        $results = $query->getResult();
-        dd($results);
+        $files = glob(self::QR_CODE_IMAGE_DIRECTORY . '/*');
+        foreach ($files as $file) { // iterate files
+            if (is_file($file)) {
+                unlink($file); // delete file
+            }
+        }
     }
 }
