d5216d5ca51ef007ad14f570c63498de2f1195bf
RedPandore <houry9@gmail.com>
2022-09-29 09:38:52 +0200

    header

--- public/theme/bocauxdeschamps/js/theme.js
+++ public/theme/bocauxdeschamps/js/theme.js
@@ -1,1 +1,14 @@
 /* Ce fichier est charger sur _master ( équivalent de base.html.twig ) */
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  menuBurger();
+});
+
+function menuBurger() {
+  const button = document.querySelector(".menu__mobile_button");
+const navbar = document.querySelector(".navbar");
+  button.addEventListener("click", () => {
+    button.classList.toggle("active");
+    navbar.classList.toggle("active");
+  });
+}
