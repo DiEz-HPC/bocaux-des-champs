11b20c8f7051c7df9ebdc5276438c621ba74f39e
RedPandore <houry9@gmail.com>
2023-02-01 14:49:07 +0100

    change docker file, add config file

---
+++ docker/entrypoint/docker-entry.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+php docker/composer.phar install --no-interaction --prefer-dist --optimize-autoloader
+
+## Symfony configuration
+
+php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
+php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
+
+
+php bin/console cache:clear
+php bin/console cache:warmup
+
+chmod -R 777 /var/www/var
+chmod -R 777 /var/www/public
+
+## server config
+php-fpm -D &
+nginx -g "daemon off;"
